a4d4424e5d69c498fdc99c61433affea1ebc2319
clempal <clement.palliere@hotmail.fr>
2014-06-20 13:56:26 +0800

    Adapt the repository specificaly for the prototype

--- zigbee/zigbee_reader.py
+++ zigbee/zigbee_reader.py
@@ -2,7 +2,7 @@ import time
 import serial
 
 from ubigate import logger
-from zigbee.sensors import pirled_signal, bedsensor_signal
+from zigbee.sensors import bedsensor_signal
 
 serialport = serial.Serial("/dev/serial/by-id/usb-FTDI_XBIB-U-DEV-if00-port0", 9600, timeout=0.1)
 
@@ -15,15 +15,12 @@ def read_zigbee():
         return None
 
 def gather_data(signal, timezone):
-    signal_types = [pirled_signal, bedsensor_signal]
-
+  
     data = None
     sensor = None
 
-    for checker in signal_types:
-        sensor, data = checker.matches(signal, timezone)
-        if data is not None:
-            break
+    sensor, data = bedsensor_signal.matches(signal, timezone)
+
     return sensor, data
 
 
