517552577f2e6c7bdfca1fb9aaf25bd2c5fed48a
clempal <clement.palliere@hotmail.fr>
2014-07-30 12:06:40 +0800

    Correction of the conf.init.default bug

--- zigbee/sensors/bed_reasoning.py
+++ zigbee/sensors/bed_reasoning.py
@@ -9,6 +9,7 @@ THRESHOLD = int(config['BEDSENSOR']['THRESHOLD'])
 def occupency(DR1):
     """
     Methode to define if the bed is occupied or not in using one single sensor
+    TODO : read the file only one time.
     """
 
     for val in ['R1','R2','R3','R4','R5','R6','R7','R8']:
