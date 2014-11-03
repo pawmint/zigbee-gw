218986532b80ff2faba994418be3cd19f3e8b29a
Romain Endelin <romain.endelin@gmail.com>
2014-11-03 14:32:23 +0100

    Better log handling with ubigate0.1.1

--- zigbee/gateway.py
+++ zigbee/gateway.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-import logging
-
 from zigbee import zigbee_IO
 
 from ubigate import Ubigate
-from ubigate import log, logger
+from ubigate import logger
 
 
 def main():
     gate = Ubigate('zigbee-gw',
                    default_file='resources/conf.json.default')
-    log.add_logger_file('data.log', logging.DEBUG)
-    logger.setLevel(logging.DEBUG)
 
     logger.info("Starting application")
     logger.info('Timezone: %s' % gate.timezone)
@@ -31,8 +27,6 @@ def main():
                 logger.warning("Unknown sensor: %s" % data['sensor'])
             else:
                 topic = "/zigbee/sensor/%s/%s" % (data['sensor'], data['type'])
-                # topic = "/marmitek/sensor/%s/%s" % (data['sensor'],
-                #                                     data['type'])
                 gate.push(data['house'], topic, data)
 
 
--- zigbee/sensors/reasoning.py
+++ zigbee/sensors/reasoning.py
@@ -3,14 +3,16 @@ def occupency(DR1, THRESHOLD):
     Method to define if the bed is occupied or not in using one single sensor
     TODO : read the file only one time.
     """
-    if DR1['R1'] == 0 and DR1['R2'] == 0 and DR1['R3'] == 0 and DR1['R4'] == 0 and DR1['R5'] == 0 and DR1['R6'] == 0 and DR1['R7'] == 0 and DR1['R8'] == 0:
-        state = "off"
-    elif DR1['R1'] > THRESHOLD and DR1['R2'] == 0 and DR1['R3'] == 0 and DR1['R4'] == 0 and DR1['R5'] == 0 and DR1['R6'] == 0 and DR1['R7'] == 0 and DR1['R8'] == 0:
-        state = "on"
+    state = "off"
 
-    return state
+    while True:
+        if(DR1['R1'] == 0 and DR1['R2'] == 0 and DR1['R3'] == 0 and
+           DR1['R4'] == 0 and DR1['R5'] == 0 and DR1['R6'] == 0 and
+           DR1['R7'] == 0 and DR1['R8'] == 0):
+            state = "off"
+        elif(DR1['R1'] > THRESHOLD and DR1['R2'] == 0 and DR1['R3'] == 0 and
+             DR1['R4'] == 0 and DR1['R5'] == 0 and DR1['R6'] == 0 and
+             DR1['R7'] == 0 and DR1['R8'] == 0):
+            state = "on"
 
-    # for val in ['R1','R2','R3','R4','R5','R6','R7','R8']:
-    #     if DR1[val] > THRESHOLD:
-    #         return "on"
-    # return "off"
+        yield state
