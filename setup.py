67b3badae0548bb429a1d2d2ef6f764a200c0ee4
Romain Endelin <romain.endelin@gmail.com>
2014-11-25 16:11:27 +0100

    Updated for ubigate1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme = open('README.md').read()
 
 setup(
     name='Zigbee-gw',
-    version='1.0.1',
+    version='1.1',
     description='A gateway to use the sensors that communicate through zigbee',
     long_description=readme,
     author='Clément Pallière',
@@ -17,12 +17,12 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'ubigate>=0.1.3',
+        'ubigate>=0.1.4',
         'pyserial>=2.7',
         'Xbee>=2.0.0'
     ],
     dependency_links=[
-        "git+ssh://git@github.com/pawmint/ubiGATE.git@v0.1.3#egg=UbiGate-0.1.3"
+        "git+ssh://git@github.com/pawmint/ubiGATE.git@v0.1.4#egg=UbiGate-0.1.4"
     ],
     entry_points={
         'console_scripts': ['zigbee-gw=zigbee.gateway:main'],
@@ -30,7 +30,7 @@ setup(
     license='Copyright',
     zip_safe=True,  # To be verified
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
